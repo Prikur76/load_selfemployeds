8e36c62fc9552427663013c00b97c4cf7bc5f6f4
Prikur76 <44672462+Prikur76@users.noreply.github.com>
2024-06-20 09:07:24 +0300

    Fix settings.py

--- settings.py
+++ settings.py
@@ -8,35 +8,35 @@ ELEMENT_PASSWORD = env.str('ELEMENT_PASSWORD')
 
 REPORT_ID = env.str('REPORT_SPREADSHEETS_ID')
 RANGE_FOR_SELFEMPLOYED = env.str('RANGE_FOR_SELFEMPLOYED')
-PARKS = [
-    {
-        'city': 'МОСКВА',
-        'id': env.str('MSK_PARK_ID'),
-        'api_key': env.str('MSK_X_API_KEY')
-    },
-    {
-        'city': 'МОСКВА (СОЦИАЛЬНЫЙ)',
-        'id': env.str('SPECIAL_PARK_ID'),
-        'api_key': env.str('SPECIAL_X_API_KEY')
-    },
-    {
-        'city': 'МОСКВА',
-        'id': env.str('ELS_PARK_ID'),
-        'api_key': env.str('ELS_X_API_KEY')
-    },
-    {
-        'city': 'Екатеринбург',
-        'id': env.str('EKB_PARK_ID'),
-        'api_key': env.str('EKB_X_API_KEY')
-    },
-    {
-        'city': 'Ярославль',
-        'id': env.str('YAR_PARK_ID'),
-        'api_key': env.str('YAR_X_API_KEY')
-    },
-    {
-        'city': 'ЧЕЛЯБИНСК',
-        'id': env.str('CHL_PARK_ID'),
-        'api_key': env.str('CHL_X_API_KEY')
-    }
-]
+# PARKS = [
+#     {
+#         'city': 'МОСКВА',
+#         'id': env.str('MSK_PARK_ID'),
+#         'api_key': env.str('MSK_X_API_KEY')
+#     },
+#     {
+#         'city': 'МОСКВА (СОЦИАЛЬНЫЙ)',
+#         'id': env.str('SPECIAL_PARK_ID'),
+#         'api_key': env.str('SPECIAL_X_API_KEY')
+#     },
+#     {
+#         'city': 'МОСКВА',
+#         'id': env.str('ELS_PARK_ID'),
+#         'api_key': env.str('ELS_X_API_KEY')
+#     },
+#     {
+#         'city': 'Екатеринбург',
+#         'id': env.str('EKB_PARK_ID'),
+#         'api_key': env.str('EKB_X_API_KEY')
+#     },
+#     {
+#         'city': 'Ярославль',
+#         'id': env.str('YAR_PARK_ID'),
+#         'api_key': env.str('YAR_X_API_KEY')
+#     },
+#     {
+#         'city': 'ЧЕЛЯБИНСК',
+#         'id': env.str('CHL_PARK_ID'),
+#         'api_key': env.str('CHL_X_API_KEY')
+#     }
+# ]
